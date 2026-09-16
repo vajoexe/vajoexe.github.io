fe8c95385a8bd4be8fca5fae5624c9170b2e08ca
eggr. <ciernyalex50@gmail.com>
2026-09-16 21:50:14 +0200

    Add new post entry for updates 1

--- blog/posts.js
+++ blog/posts.js
@@ -1,4 +1,5 @@
 const posts = [
+    { title: "updates 1.", date: "16/9/2026", file: "posts/updates 1..md" },
     { title: "[BTS] - concrete structure", date: "16/9/2026", file: "posts/[BTS] - concrete structure.md" },
     { title: "massive update!", date: "16/9/2026", file: "posts/massive update!.md" }
 ];
