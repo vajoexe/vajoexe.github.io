6b7e17ad0c538d35d641c2d78bf84ea3fc6ff9cd
eggr. <ciernyalex50@gmail.com>
2026-09-16 18:08:57 +0200

    Fix formatting issue in posts.js

--- blog/posts.js
+++ blog/posts.js
@@ -1,3 +1,3 @@
 const posts = [
-    { title: "massive update!", date: "16/9/2026", file: "posts/massive update!.md" },
+    { title: "massive update!", date: "16/9/2026", file: "posts/massive update!.md" }
 ];
