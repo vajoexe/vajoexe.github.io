8a8d9c9f6b74de53753894b412ad83de043895e4
vajoexe <ciernyalex50@gmail.com>
2026-09-25 13:42:40 +0200

    added changes 3. in blog

--- blog/posts.js
+++ blog/posts.js
@@ -1,4 +1,5 @@
 const posts = [
+    { title: "updates 3.", date: "25/9/2026", file: "posts/updates 3..md" },
     { title: "fh6 custom loading screen", date: "21/9/2026", file: "posts/fh6 custom loading screen.md" },
     { title: "updates 2.", date: "18/9/2026", file: "posts/updates 2..md" },
     { title: "updates 1.", date: "16/9/2026", file: "posts/updates 1..md" },
---
+++ blog/posts/updates 3..md
@@ -0,0 +1,5 @@
+• remade the main "panel" noise to be randomly generated in real time, gives a more "cinematic look" or whatever
+
+• made procedural noise generate at a 30fps cap to mainly help mobile devices handle the demanding task of Math.random() in js
+
+only a small update, but hope that is it for today.
